0510965f58b78b67540e4379258df147f0ffdbbf
Muhammad Daffa <daffa.audivie27@gmail.com>
2026-01-26 16:09:55 +0700

    [FIX] Update UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,41 +16,41 @@ class UserSeeder extends Seeder
         User::create([
             'nama' => 'Administrator',
             'email' => 'admin@gmail.com',
-            'password' => Hash::make('abcd1234'),
+            'password' => Hash::make('kalasubaindonesia'),
             'id_cabang' => null, 
             'role' => Role::ADMIN,
             'email_verified_at' => now(),
         ]);
 
-        $cabangDesapa = Cabang::where('nama_cabang', 'Desapa Resto')->firstOrFail();
-        $cabangKsiMalang = Cabang::where('nama_cabang', 'KSI AC Mobil Malang')->firstOrFail();
-        $cabangCobanKethak = Cabang::where('nama_cabang', 'Coban Kethak')->firstOrFail();
+        // $cabangDesapa = Cabang::where('nama_cabang', 'Desapa Resto')->firstOrFail();
+        // $cabangKsiMalang = Cabang::where('nama_cabang', 'KSI AC Mobil Malang')->firstOrFail();
+        // $cabangCobanKethak = Cabang::where('nama_cabang', 'Coban Kethak')->firstOrFail();
 
-        User::create([
-            'nama' => 'Staff Desapa',
-            'email' => 'desapa@gmail.com',
-            'password' => Hash::make('abcd1234'),
-            'id_cabang' => $cabangDesapa->id,
-            'role' => Role::STAFF,
-            'email_verified_at' => now(),
-        ]);
+        // User::create([
+        //     'nama' => 'Staff Desapa',
+        //     'email' => 'desapa@gmail.com',
+        //     'password' => Hash::make('abcd1234'),
+        //     'id_cabang' => $cabangDesapa->id,
+        //     'role' => Role::STAFF,
+        //     'email_verified_at' => now(),
+        // ]);
 
-        User::create([
-            'nama' => 'Staff KSI Malang',
-            'email' => 'ksimalang@gmail.com',
-            'password' => Hash::make('abcd1234'),
-            'id_cabang' => $cabangKsiMalang->id,
-            'role' => Role::STAFF,
-            'email_verified_at' => now(),
-        ]);
+        // User::create([
+        //     'nama' => 'Staff KSI Malang',
+        //     'email' => 'ksimalang@gmail.com',
+        //     'password' => Hash::make('abcd1234'),
+        //     'id_cabang' => $cabangKsiMalang->id,
+        //     'role' => Role::STAFF,
+        //     'email_verified_at' => now(),
+        // ]);
 
-        User::create([
-            'nama' => 'Staff Coban kethak',
-            'email' => 'kethak@gmail.com',
-            'password' => Hash::make('abcd1234'),
-            'id_cabang' => $cabangCobanKethak->id,
-            'role' => Role::STAFF,
-            'email_verified_at' => now(),
-        ]);
+        // User::create([
+        //     'nama' => 'Staff Coban kethak',
+        //     'email' => 'kethak@gmail.com',
+        //     'password' => Hash::make('abcd1234'),
+        //     'id_cabang' => $cabangCobanKethak->id,
+        //     'role' => Role::STAFF,
+        //     'email_verified_at' => now(),
+        // ]);
     }
 }
